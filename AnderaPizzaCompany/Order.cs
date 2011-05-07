40517950b947eee322209cb90526de445f4393c9
Topher Brown <topher@tophernet.com>
2011-05-07 01:00:58 -0400

    adding class Order. only has reference number

---
+++ AnderaPizzaCompany/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnderaPizzaCompany
+{
+    public class Order
+    {
+        private int reference_number;
+
+        public Order()
+        {
+            reference_number = 1337;
+        }
+    }
+}
