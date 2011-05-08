08b6904fd7258869b890d3e2438a85b4d950120f
Topher Brown <topher@tophernet.com>
2011-05-08 15:22:31 -0400

    Pizza.cs: topping list

--- AnderaPizzaCompany/Pizza.cs
+++ AnderaPizzaCompany/Pizza.cs
@@ -7,5 +7,15 @@ namespace AnderaPizzaCompany
 {
     public class Pizza
     {
+        public List<string> toppings;
+        private Pizza()
+        {
+            toppings = new List<string>();
+        }
+
+        public void AddTopping(string topping)
+        {
+            toppings.Add(topping);
+        }
     }
 }
