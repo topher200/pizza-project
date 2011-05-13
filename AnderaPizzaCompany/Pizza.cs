0d9148a7c1679b0694013c79ee740815a8a70de4
Topher Brown <topher@tophernet.com>
2011-05-13 02:51:18 -0400

    creating Topping class

--- AnderaPizzaCompany/Pizza.cs
+++ AnderaPizzaCompany/Pizza.cs
@@ -10,17 +10,17 @@ namespace AnderaPizzaCompany
         public String name;
         public String base_toppings;
         public decimal cost;
-        public List<string> toppings;
+        public List<Topping> toppings;
 
         public Pizza(String name, String base_toppings, decimal cost)
         {
             this.name = name;
             this.base_toppings = base_toppings;
             this.cost = cost;
-            toppings = new List<string>();
+            toppings = new List<Topping>();
         }
 
-        public void AddTopping(string topping)
+        public void AddTopping(Topping topping)
         {
             toppings.Add(topping);
         }
---
+++ AnderaPizzaCompany/Topping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnderaPizzaCompany
+{
+    public class Topping
+    {
+        public string name;
+        public decimal cost;
+
+        public Topping(string name, decimal cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+    }
+}
