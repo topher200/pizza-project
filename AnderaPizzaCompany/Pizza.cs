bdc5e2b050cd1cfd4bdb8423eeaf901bb9921002
Topher Brown <topher@tophernet.com>
2011-05-13 03:33:17 -0400

    Pizza: added Cost()

--- AnderaPizzaCompany/Pizza.cs
+++ AnderaPizzaCompany/Pizza.cs
@@ -9,20 +9,30 @@ namespace AnderaPizzaCompany
     {
         public String name;
         public String base_toppings;
-        public decimal cost;
+        public decimal base_cost;
         public List<Topping> toppings;
 
-        public Pizza(String name, String base_toppings, decimal cost)
+        public Pizza(String name, String base_toppings, decimal base_cost)
         {
             this.name = name;
             this.base_toppings = base_toppings;
-            this.cost = cost;
+            this.base_cost = base_cost;
             toppings = new List<Topping>();
         }
 
         public void AddTopping(Topping topping)
         {
             toppings.Add(topping);
+        }
+
+        public decimal Cost()
+        {
+            decimal cost = this.base_cost;
+            foreach (Topping t in toppings)
+            {
+                cost += t.cost;
+            }
+            return cost;
         }
     }
 }
